500615e6cbdc578ae793abf450dfeff2b16d7d52
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2021-05-17 11:14:35 +0700

    add project

---
+++ Sport Class Online Back-end/src/main/java/com/example/onlineclasswebapp/entity/Course.java
@@ -0,0 +1,10 @@
+package com.example.onlineclasswebapp.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class Course {
+    public int courseId;
+    public String courseName;
+}
---
+++ Sport Class Online Back-end/src/main/java/com/example/onlineclasswebapp/entity/User.java
@@ -0,0 +1,12 @@
+package com.example.onlineclasswebapp.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class User {
+    public int userId;
+    public String userPhone;
+    public String userEmail;
+    public String userPassword;
+}
