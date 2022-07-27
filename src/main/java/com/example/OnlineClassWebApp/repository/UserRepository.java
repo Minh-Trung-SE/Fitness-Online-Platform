5283f9775b95b88c32151939e58dd60f47de9b87
Minh Trung <nguyenminhtrungse@gmail.com>
2022-07-28 04:01:28 +0700

    Clear project maven

--- src/main/java/com/example/OnlineClassWebApp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.OnlineClassWebApp.repository;
-
-import com.example.OnlineClassWebApp.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByUserPhone(String userPhone);
-    User findByUserPhoneAndUserEmail(String userPhone, String userEmail);
-}
